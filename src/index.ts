7ec0912056933a7f0afe7acf47684a2728bf93bc
skarab42 <skarab@bluewin.ch>
2022-07-06 09:09:28 +0200

    feat: type check

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,65 @@
 import type { Plugin } from 'vite';
+import { typeCheck, type DiagnosticMessage } from './compiler';
 
 export function vitestTypescriptAssertPlugin(): Plugin {
 	return {
 		name: 'vitest:typescript-assert',
 		apply: 'serve',
 		enforce: 'pre',
+		transform(code, id) {
+			if (!id.endsWith('.test.ts')) {
+				// TODO get from config, with glob pattern
+				return;
+			}
+
+			return `${code}${error(typeCheck(id))}`;
+		},
 	};
+}
+
+function error(messages?: DiagnosticMessage[]) {
+	const fistMessage = messages ? messages[0] : undefined;
+
+	if (!fistMessage) {
+		return '';
+	}
+
+	const file = String(fistMessage.file);
+	const line = Number(fistMessage.line) + 1;
+	const column = Number(fistMessage.column) * 2;
+
+	return `\ntest.concurrent('type-check', () => {throw error()});
+
+		function error() {
+			const err = new TypeError("${fistMessage.message} ts(${fistMessage.code})");
+		
+			err.name = "TypeError";
+			err.nameStr = "TypeError";
+
+			// err.showDiff = true;
+			// err.actual = "life";
+			// err.expected = "42";
+
+			// err.stackStr = "???";
+			// err.operator = "???";
+			// err.type = "???";
+			
+			err.stacks = [{
+				method: "",
+				file: "${file}",
+				line: ${line},
+				column: ${column},
+				sourcePos: {
+					source: "${file}",
+					line: ${line},
+					column: ${column},
+				}
+			}]
+
+			// console.log(err.stack)
+			// console.log(err.stacks)
+
+			return err;
+		}
+	`;
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -3,7 +3,16 @@ import { expect, test } from 'vitest';
 test('test-1', () => {
 	expect(42).toBe(42);
 	expect(42).toBe(25); // <- fail
+});
+
+test('test-2', () => {
+	expect(true).toBe(false); // <- fail
 
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const prout = 'pouet'; // <- shoul fail
+	const prout = 'pouet'; // <- should fail
+
+	// eslint-disable-next-line no-console
+	console.log('plop'); // <- should fail
+
+	throw new Error('Prout'); // <- fail
 });
